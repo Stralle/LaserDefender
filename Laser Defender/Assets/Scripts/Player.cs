84c6e6454be6b16fb01bacdaf618fa3520ad94cb
Stralle <strahinja.rodic@hotmail.com>
2020-02-28 21:25:11 +0100

    Added player movement

---
+++ Laser Defender/Assets/Scripts/Player.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    float m_MoveSpeed = 10.0f;
+
+    [SerializeField]
+    float m_Padding = 1.0f;
+
+    // Local variables
+    float m_xMin = 0.0f;
+    float m_yMin = 0.0f;
+    float m_xMax = 0.0f;
+    float m_yMax = 0.0f;
+
+    private void Start()
+    {
+        SetupMoveBoundaries();
+    }
+
+    void Update()
+    {
+        Move();
+     
+    }
+
+    private void SetupMoveBoundaries()
+    {
+        Camera gameCamera = Camera.main;
+
+        m_xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + m_Padding;
+        m_xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - m_Padding;
+
+        m_yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + m_Padding;
+        m_yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - m_Padding;
+    }
+
+    private void Move()
+    {
+        float deltaX = m_MoveSpeed * Time.deltaTime * Input.GetAxis("Horizontal");
+        float deltaY = m_MoveSpeed * Time.deltaTime * Input.GetAxis("Vertical");
+
+        float newXPos = Mathf.Clamp(transform.position.x + deltaX, m_xMin, m_xMax);
+        float newYPos = Mathf.Clamp(transform.position.y + deltaY, m_yMin, m_yMax);
+        transform.position = new Vector2(newXPos, newYPos);
+    }
+}
