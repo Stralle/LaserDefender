afe903387784b97f5f865a53cdcc41f1635ff45d
Strahinja <srodic16@raf.edu.rs>
2020-03-19 23:13:07 +0100

    Added enemy wave configuration. Created 2 enemy waves

---
+++ Laser Defender/Assets/Scripts/EnemyPathing.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathing : MonoBehaviour
+{
+    [SerializeField]
+    float m_MoveSpeed = 2.0f;
+
+    [SerializeField]
+    WaveConfig m_WaveConfig;
+
+    List<Transform> m_Waypoints = new List<Transform>();
+    int m_WaypointIndex = 0;
+
+    void Start()
+    {
+        m_Waypoints = m_WaveConfig.GetWaypoints();
+        transform.position = m_Waypoints[m_WaypointIndex].transform.position;
+        Debug.Log("Waypoints count: " + m_Waypoints.Count);
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (m_WaypointIndex < m_Waypoints.Count)
+        {
+            var targetPosition = m_Waypoints[m_WaypointIndex].transform.position;
+            var movementThisFrame = m_MoveSpeed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, movementThisFrame);
+
+            if (transform.position == targetPosition)
+            {
+                m_WaypointIndex++;
+                Debug.Log(m_WaypointIndex);
+            }
+        }
+        else
+        {
+            Destroy(gameObject); 
+            Debug.Log("Destroy " + m_WaypointIndex);
+        }
+    }
+}
---
+++ Laser Defender/Assets/Scripts/WaveConfig.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Enemy Wave Config")]
+public class WaveConfig : ScriptableObject
+{
+    [SerializeField]
+    GameObject m_EnemyPrefab;
+
+    [SerializeField]
+    GameObject m_PathPrefab;
+
+    [SerializeField]
+    float m_TimeBetweenSpawns = 0.5f;
+
+    [SerializeField]
+    float m_SpawnRandomFactor = 0.3f;
+
+    [SerializeField]
+    int m_NumberOfEnemies = 5;
+
+    [SerializeField]
+    float m_MoveSpeed = 2.0f;
+
+    public GameObject GetEnemyPrefab() { return m_EnemyPrefab; }
+    public List<Transform> GetWaypoints()
+    {
+        List<Transform> waveWaypoints = new List<Transform>();
+
+        foreach (Transform child in m_PathPrefab.transform)
+        {
+            waveWaypoints.Add(child);
+        }
+
+        return waveWaypoints; 
+    }
+    public float GetTimeBetweenSpawns() { return m_TimeBetweenSpawns; }
+    public float GetSpawnRandomFactor() { return m_SpawnRandomFactor; }
+    public int GetNumberOfEnemies() { return m_NumberOfEnemies; }
+    public float GetMoveSpeed() { return m_MoveSpeed; }
+
+
+}
