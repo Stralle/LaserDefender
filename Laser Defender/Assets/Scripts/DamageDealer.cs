875d4c244ef1a4a9b3556bb75a1c2f8eb6615253
Stralle <strahinja.rodic@hotmail.com>
2020-03-25 21:58:36 +0100

    Adding dealing damage to an enemy

---
+++ Laser Defender/Assets/Scripts/DamageDealer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageDealer : MonoBehaviour
+{
+    [SerializeField]
+    int m_Damage = 100;
+
+    public int GetDamage()
+    {
+        return m_Damage;
+    }
+
+    public void Hit()
+    {
+        Destroy(gameObject);
+    }
+
+}
---
+++ Laser Defender/Assets/Scripts/Enemy.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    float m_Health = 100;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        DamageDealer damageDealer = collision.GetComponent<DamageDealer>();
+        m_Health -= damageDealer.GetDamage();
+    }
+}
